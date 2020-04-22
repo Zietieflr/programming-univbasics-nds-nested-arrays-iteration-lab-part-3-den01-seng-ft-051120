abc1e76b2d1c86762191d7fa88ba17035c6bc278
Logan McGuire <lrmcguire93@gmail.com>
2020-04-22 21:04:51 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,8 @@ def join_nested_strings(src)
   final_message = "";
   src.length.times do |index|
     src.length.times do |index2|
-      if src[index][index2].is_a?
+      if src[index][index2].is_a?(String)
+        final_message + src[index][index2];
     end
   end
 end
